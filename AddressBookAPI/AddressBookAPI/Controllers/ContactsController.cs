3fc1b89ff030774eafd4dec1375b3ce75d8e7f31
Ersin İşgör <ersinisgor@hotmail.com>
2023-11-28 23:35:54 +0300

    Change controller for ContactService

--- AddressBookAPI/AddressBookAPI/Controllers/ContactsController.cs
+++ AddressBookAPI/AddressBookAPI/Controllers/ContactsController.cs
@@ -1,4 +1,5 @@
 ﻿using AddressBookAPI.Models;
+using AddressBookAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,24 +10,19 @@ namespace AddressBookAPI.Controllers
 	[ApiController]
 	public class ContactsController : ControllerBase
 	{
-		private static List<Contact> contacts = new List<Contact>()
-		{
-			new Contact(){Id=1, FirstName="Jack", LastName="Jackson", PhoneNumber="111-111-1111", Address="111 Main Street, Minneapolis, MN 55001"},
-			new Contact(){Id=2, FirstName="John", LastName="Johnson", PhoneNumber="222-222-2222", Address="222 Main Street, Minneapolis, MN 55001"},
-			new Contact(){Id=3, FirstName="Mary", LastName="Erickson", PhoneNumber="333-333-3333", Address="333 Main Street, Minneapolis, MN 55001"}
-		};
-
 
 		[HttpGet]
 		public IActionResult AllContacts()
 		{
-			return Ok(contacts);
+			var service = new ContactService();
+			return Ok(service.GetAllContacts());
 		}
 
 		[HttpGet("{id}")]
 		public IActionResult ContactById(int id)
 		{
-			var contact = contacts.FirstOrDefault(x => x.Id == id);
+			var service = new ContactService();
+			var contact = service.GetContactById(id);
 			if (contact == null)
 			{
 				return BadRequest("Can not find this contact");
@@ -37,13 +33,14 @@ namespace AddressBookAPI.Controllers
 		[HttpPost]
 		public IActionResult CreateContact([FromBody] Contact newContact)
 		{
-			var contact = new Contact();
-			contact.Id = newContact.Id;
-			contact.FirstName = newContact.FirstName;
-			contact.LastName = newContact.LastName;
-			contact.PhoneNumber = newContact.PhoneNumber;
-			contact.Address = newContact.Address;
-			contacts.Add(contact);
+			var service = new ContactService();
+
+			var contact = service.CreateContact(newContact);
+			if (contact == null)
+			{
+				return BadRequest("Can not find this contact to update");
+			}
+
 
 			return Ok(contact);
 		}
@@ -51,16 +48,13 @@ namespace AddressBookAPI.Controllers
 		[HttpPut]
 		public IActionResult UpdateContact([FromBody] Contact updateContact)
 		{
-			var contact = contacts.FirstOrDefault(x => x.Id == updateContact.Id);
+			var service = new ContactService();
+
+			var contact = service.UpdateContact(updateContact);
 			if (contact == null)
 			{
 				return BadRequest("Can not find this contact to update");
 			}
-
-			contact.FirstName = updateContact.FirstName;
-			contact.LastName = updateContact.LastName;
-			contact.PhoneNumber = updateContact.PhoneNumber;
-			contact.Address = updateContact.Address;
 
 			return Ok(contact);
 
@@ -69,13 +63,15 @@ namespace AddressBookAPI.Controllers
 		[HttpDelete("{id}")]
 		public IActionResult DeleteContact(int id)
 		{
-			var contact = contacts.FirstOrDefault(x => x.Id == id);
-			if (contact == null)
+			var service = new ContactService();
+
+			var result = service.DeleteContact(id);
+			if (result == false)
 			{
 				return BadRequest("Can not find this contact to delete");
 			}
 
-			contacts.Remove(contact);
+			
 			return Ok("The contact has been deleted");
 		}
 	}
